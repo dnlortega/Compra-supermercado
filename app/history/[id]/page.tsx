738f2f9943625db9710c35ab6439e5ce239d09de
dnlortega <dnlortega@gmail.com>
2025-12-17 15:35:37 -0300

    feat: add dynamic history detail page and upgrade ESLint to v9

--- app/history/[id]/page.tsx
+++ app/history/[id]/page.tsx
@@ -1,5 +1,4 @@
-import HistoryDetailClient from "./history-detail-client";
-
-export default function HistoryDetailPage({ params }: { params: { id: string } }) {
-    return <HistoryDetailClient listId={params.id} />;
+export default async function HistoryDetailPage({ params }: { params: Promise<{ id: string }> }) {
+    const { id } = await params;
+    return <HistoryDetailClient listId={id} />;
 }
